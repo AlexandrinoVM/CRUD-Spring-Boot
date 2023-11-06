55641da4058e1c642f3759d0662084a1575259a1
alexandrinoVM <vinicius.moraes2@outlook.com>
2023-11-06 16:42:31 -0300

    styles

--- src/main/java/com/example/demo/services/cargoService.java
+++ src/main/java/com/example/demo/services/cargoService.java
@@ -1,9 +1,12 @@
 package com.example.demo.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.models.cargoModel;
+import com.example.demo.models.funcionarioModel;
 import com.example.demo.repository.cargoRepository;
 
 @Service
@@ -11,7 +14,10 @@ public class cargoService {
     @Autowired
     cargoRepository cargoRepository;
 
-
+    public Optional<cargoModel> findById(Integer id){
+        return cargoRepository.findById(id);
+       
+    }
     public void save(cargoModel cargo){
         cargoRepository.save(cargo);
     }
